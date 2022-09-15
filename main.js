7b2f806a70eea99bf2bc294c233842fd927d0413
Lydia <lydiadoula@gmail.com>
2022-09-15 21:00:10 +0100

    added list of people

---
+++ main.js
@@ -0,0 +1,39 @@
+let people = [
+    {
+    name: 'Hanan Wadworth', //string
+    dateOfBirth: 1/15/1987, //number
+    children: 3, //number
+    country: 'China', //string
+    knowsHowToProgram: false //boolean
+    },
+    {
+    name: 'Annelise Gavahan', //string
+    dateOfBirth: 3/9/2001, //number
+    children: 3, //number
+    country: 'Germany', //string
+    knowsHowToProgram: false //boolean
+    },
+    {
+    name: 'Genevieve Barnhart', //string
+    dateOfBirth: 1/12/1986, //number
+    children: 2, //number
+    country: 'Myanmar', //string
+    knowsHowToProgram: true //boolean
+    },
+    {
+    name: 'Rachelle Coltart', //string
+    dateOfBirth: 11/11/1987, //number
+    children: 0, //number
+    country: 'China', //string
+    knowsHowToProgram: false //boolean
+    },
+    {
+    name: 'Janelle Droogan', //string
+    dateOfBirth: 4/21/1994, //number
+    children: 3, //number
+    country: 'Norway', //string
+    knowsHowToProgram: true //boolean
+    },
+]
+
+console.log(people);
